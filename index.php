ab7c07ccf95a19f698b07b6160fdea5a44f85664
MDAlviano <mdalviano.21122@gmail.comgit initgit config --global user.email mdalviano.21122@gmail.com>
2024-01-28 06:28:37 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$hello = "Hello World!";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>
+        <?= $hello; ?>
+    </h1>
+</body>
+</html>
